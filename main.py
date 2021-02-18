785c7f62bb1474d26dee56babf5fd29fcd7cef61
Brandon <juhkim90@gmail.com>
2021-02-18 11:19:24 +0000

    Initial commit

---
+++ main.py
@@ -0,0 +1,127 @@
+for x in range(3):
+	print (x)
+
+for y in range(2, 6):
+	print (y)
+
+for z in range(-4, 7, 2):
+	print (z)
+
+
+
+'''
+# Exercise 1
+# Outputs: 2x hello, 2x new year, 1x 2021
+
+for x in range(_____):
+	if (_____ < _____):
+		print ("hello")
+	elif (_____ < _____):
+		print ("Python")
+	elif (_____ < _____):
+		print ("new year")
+	elif (_____ < _____):
+		print ("bye")
+	else:
+		print ("2021")
+
+
+# Exercise 2
+# Outputs: 1x hello, 2x Python, 1x new year, 1x bye, 2x 2021
+
+for x in range(4, _____):
+	if (_____ < _____):
+		print ("hello")
+	elif (_____ < _____):
+		print ("Python")
+	elif (_____ < _____):
+		print ("new year")
+	elif (_____ < _____):
+		print ("bye")
+	else:
+		print ("2021")
+
+
+# Exercise 3
+# Outputs: 2x hello, 1x Python, 1x new year, 2x bye, 1x 2021
+
+for x in range(7, _____, 3):
+	if (_____ < _____):
+		print ("hello")
+	elif (_____ < _____):
+		print ("Python")
+	elif (_____ < _____):
+		print ("new year")
+	elif (_____ < _____):
+		print ("bye")
+	else:
+		print ("2021")
+'''
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+'''
+# Answer to Exercise 1
+# Outputs: 2x hello, 2x new year, 1x 2021
+
+for x in range(5):
+	if (x < 2):
+		print ("hello")
+	elif (x < 2):
+		print ("Python")
+	elif (x < 4):
+		print ("new year")
+	elif (x < 4):
+		print ("bye")
+	else:
+		print ("2021")
+
+
+# Answer to Exercise 2
+# Outputs: 1x hello, 2x Python, 1x new year, 1x bye, 2x 2021
+
+for x in range(4, 11):
+	if (x < 5):
+		print ("hello")
+	elif (x < 7):
+		print ("Python")
+	elif (x < 8):
+		print ("new year")
+	elif (x < 9):
+		print ("bye")
+	else:
+		print ("2021")
+
+
+# Answer to Exercise 3
+# Outputs: 2x hello, 1x Python, 1x new year, 2x bye, 1x 2021
+
+for x in range(7, 28, 3):
+	if (x < 13):
+		print ("hello")
+	elif (x < 16):
+		print ("Python")
+	elif (x < 19):
+		print ("new year")
+	elif (x < 24):
+		print ("bye")
+	else:
+		print ("2021")
+'''
